a1dec81f6c89d24795e3baaf8f57ae240f883301
camealion <heffalatapus@gmail.com>
2011-12-18 09:00:14 -0500

    Release 1.4

---
+++ ElvUI_Factionizer_Skin.lua
@@ -0,0 +1,95 @@
+﻿local function desat(self, event, ...)
+	for i = 1, 11 do 
+		cDesaturate(_G["ReputationBar"..i.."ExpandOrCollapseButton"])
+	end
+end
+
+--Factionizer by Urbin - Added by Camealion - Start
+local SkinFactionizer = CreateFrame("Frame")
+	SkinFactionizer:RegisterEvent("PLAYER_ENTERING_WORLD")
+	SkinFactionizer:SetScript("OnEvent", function(self, event, addon)
+	if IsAddOnLoaded("Skinner") or IsAddOnLoaded("Aurora") then return end
+	if not IsAddOnLoaded("Factionizer") then return end
+
+				local StripAllTextures = {
+                "FIZ_ReputationDetailFrame",
+                "FIZ_OptionsFrame",
+				}
+
+	local SetTemplateT = {-- Transparent Texture
+                "FIZ_OptionsFrame",
+                "FIZ_ReputationDetailFrame",
+				}	
+	
+	local buttons = {
+                "FIZ_OptionsButton",
+                "FIZ_ShowAllButton",
+                "FIZ_ExpandButton",
+                "FIZ_ShowNoneButton",
+				"FIZ_CollapseButton",
+				"FIZ_SupressNoneFactionButton",
+				"FIZ_SupressNoneGlobalButton",
+				"FIZ_ClearSessionGainButton",
+				}
+	
+	local checkboxes = {
+                "FIZ_OrderByStandingCheckBox",
+                "FIZ_ShowQuestButton",
+                "FIZ_ShowInstancesButton",
+                "FIZ_ShowMobsButton",
+				"FIZ_ShowItemsButton",
+				"FIZ_ShowGeneralButton",
+				"FIZ_ReputationDetailInactiveCheckBox",
+				"FIZ_ReputationDetailMainScreenCheckBox",
+				"FIZ_SupressOriginalGainBox",
+				"FIZ_ShowPreviewRepBox",
+				"FIZ_SwitchFactionBarBox",
+				"FIZ_SilentSwitchBox",
+				"FIZ_NoGuildSwitchBox",
+				"FIZ_GuildCapBox",
+				"FIZ_EnableMissingBox",
+				"FIZ_ExtendDetailsBox",
+				"FIZ_GainToChatBox",
+				"FIZ_NoGuildGainBox",
+				"FIZ_ReputationDetailAtWarCheckBox",	--Doesn't work right!															
+				}
+							
+				for _, object in pairs(StripAllTextures) do
+							_G[object]:StripTextures()
+				end	
+
+				for _, object in pairs(SetTemplateT) do
+							_G[object]:SetTemplate("Transparent")
+				end			
+	
+	-- Skin Buttons
+				for _, button in pairs(buttons) do
+								cSkinButton(_G[button])
+				end		
+				
+				for _, checkbox in pairs(checkboxes) do
+								cSkinCheckBox(_G[checkbox])
+				end	
+				
+				FIZ_OptionsFrame:CreateShadow("Default")
+
+	-- Skin Close Buttons
+				cSkinCloseButton(FIZ_OptionsFrameClose)
+				cSkinCloseButton(FIZ_ReputationDetailCloseButton)
+				
+	-- Skin Scrollbar	
+			cSkinScrollBar(FIZ_UpdateListScrollFrameScrollBar, 5)
+			
+			repFrame = _G["ReputationFrame"]
+			repFrame:SetScript("OnUpdate", desat)
+			
+	-- Set positions/sizes
+				FIZ_OptionsFrameClose:Point("TOPRIGHT", FIZ_OptionsFrame, "TOPRIGHT", 4, 5)
+                FIZ_OptionsFrame:SetWidth(360)
+				FIZ_ReputationDetailCloseButton:Point("TOPRIGHT", FIZ_ReputationDetailFrame, "TOPRIGHT", 4, 5)
+				FIZ_ShowNoneButton:Point("TOPLEFT", FIZ_ReputationDetailDivider3, "BOTTOMLEFT", 230, -5)
+				FIZ_SupressNoneGlobalButton:Point("TOPLEFT",  FIZ_SupressNoneFactionButton, "BOTTOMLEFT", 0, -5)
+				FIZ_ReputationDetailFrame:Point("TOPLEFT",   ReputationFrame, "TOPRIGHT", 5, 0)
+				FIZ_OptionsFrame:Point("TOPLEFT",   ReputationFrame, "TOPRIGHT", 5, 0)
+--Factionizer by Urbin - Added by Camealion - End
+end)
